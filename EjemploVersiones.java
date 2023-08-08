ea0bb652bfbfd142374db9ac70cafd6a4c2c92ad
seba197878 <sebav1978@gmail.com>
2023-08-08 11:37:16 -0300

    Actualice Sout

--- EjemploVersiones.java
+++ EjemploVersiones.java
@@ -15,6 +15,8 @@ public class EjemploVersiones {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        System.out.println("Prueba de Clonacion");
         // TODO code application logic here
     }
     

b9dbcdbbbd2ec1eb9acf55d1780dda3c6f1caddf
seba197878 <sebav1978@gmail.com>
2023-08-08 11:12:54 -0300

    Initial commit

---
+++ Cliente.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploversiones;
+
+/**
+ *
+ * @author seba
+ */
+public class Cliente {
+    
+    private int codigo;
+    private String descripcion;
+}
